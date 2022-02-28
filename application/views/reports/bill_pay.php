9a8ce9f81600c75c432bebb8d25118431bd42b33
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2022-02-28 18:31:21 +0800

    UI

---
+++ application/views/reports/bill_pay.php
@@ -0,0 +1,119 @@
+<div class="main-panel">
+    <div class="content-wrapper">    
+        
+        <div class="page-header">
+            <h3 class="page-title">
+                <span class="page-title-icon bg-gradient-success text-white mr-2">
+                  <i class="mdi mdi-file-document"></i>
+                </span> Reports
+            </h3>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>masterfile/purpose_list">Billing Statement</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Pay</li>
+                </ol>
+            </nav>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header bg-gradient-success card-img-holder text-white pt-2" >
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action = "">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <table class="table table-bordered table-hover">
+                                        <tr>
+                                            <td>DR Date</td>
+                                            <td>DR No</td>
+                                            <td>Total Amount</td>
+                                        </tr>
+                                        <tr>
+                                            <td> &nbsp; July 20, 2022</td>
+                                            <td> &nbsp; DR-29938773-8882</td>
+                                            <td align="right">P 67775 &nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td> &nbsp; July 20, 2022</td>
+                                            <td> &nbsp; DR-29938773-8882</td>
+                                            <td align="right">P 67775 &nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td> &nbsp; July 20, 2022</td>
+                                            <td> &nbsp; DR-29938773-8882</td>
+                                            <td align="right">P 67775 &nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td> &nbsp; July 20, 2022</td>
+                                            <td> &nbsp; DR-29938773-8882</td>
+                                            <td align="right">P 67775 &nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td> &nbsp; July 20, 2022</td>
+                                            <td> &nbsp; DR-29938773-8882</td>
+                                            <td align="right">P 67775 &nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td> &nbsp; July 20, 2022</td>
+                                            <td> &nbsp; DR-29938773-8882</td>
+                                            <td align="right">P 67775 &nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td> &nbsp; July 20, 2022</td>
+                                            <td> &nbsp; DR-29938773-8882</td>
+                                            <td align="right">P 67775 &nbsp;</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="col-lg-3">
+                                    <br>
+                                    <div class="form-group" >
+                                        <label for="exampleInputName1">Payment</label>
+                                        <div class="form-check m-0" >
+                                            <label class="form-check-label">
+                                            <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value=""> Partial Payment <i class="input-helper"></i></label>
+                                        </div>
+                                        <div class="form-check m-0">
+                                            <label class="form-check-label">
+                                            <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2" checked="">Full Payment<i class="input-helper"></i></label>
+                                        </div>
+                                    </div> 
+                                    <br>
+                                    <div class="form-group" >
+                                        <label for="exampleInputName1">Form of Payment</label>
+                                        <div class="form-check m-0" >
+                                            <label class="form-check-label">
+                                            <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value=""> Check <i class="input-helper"></i></label>
+                                        </div>
+                                        <div class="form-check m-0">
+                                            <label class="form-check-label">
+                                            <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2" checked="">Cash <i class="input-helper"></i></label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="exampleInputName1">Check No.</label>
+                                        <input type="text" class="form-control" name="" placeholder="">
+                                    </div>  
+                                    <div class="form-group">
+                                        <label for="exampleInputName1">Collection Receipt No</label>
+                                        <input type="text" class="form-control" name="" placeholder="">
+                                    </div>  
+                                    <div class="form-group">
+                                        <label for="exampleInputName1">Amount</label>
+                                        <input type="text" class="form-control" name="" placeholder="00">
+                                    </div> 
+                                    <button type="submit" class="btn btn-info btn-block">Pay</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </div>
+</div>
+        
