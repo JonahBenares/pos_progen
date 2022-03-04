16b35629a0b3f3367c7c93dbf59b5ef709b03630
jbenares <jonahbenares@gmail.com>
2022-03-04 09:20:58 +0800

    receive transaction

--- application/controllers/Receive.php
+++ application/controllers/Receive.php
@@ -54,6 +54,38 @@ class Receive extends CI_Controller {
         $this->load->view('template/footer');
     }
 
+    public function add_receive_head_process(){
+       
+     
+        $data=array(
+            "receive_date"=>$this->input->post('receive_date'),
+            "po_no"=>$this->input->post('po_no'),
+            "dr_no"=>$this->input->post('dr_no'),
+            "si_no"=>$this->input->post('si_no'),
+            "pcf"=>$this->input->post('pcf'),
+            "overall_remarks"=>$this->input->post('remarks')
+        );
+     
+
+       $id= $this->super_model->insert_return_id("receive_head", $data);
+
+       $data_details = array(
+            "receive_id"=>$id
+       );
+       $details_id= $this->super_model->insert_return_id("receive_details", $data_details);
+
+      
+       $return = array('receive_id'=>$id, 'rd_id'=>$details_id);
+       echo json_encode($return);
+    }
+
+    public function cancel_receive(){
+        $id = $this->input->post('id');
+        $this->super_model->delete_where("receive_head", "receive_id", $id);
+        $this->super_model->delete_where("receive_details", "receive_id", $id);
+        $this->super_model->delete_where("receive_items", "receive_id", $id);
+    }
+
     public function add_receive_pr()
     {
         $this->load->view('template/header');
--- assets/js/receive.js
+++ assets/js/receive.js
@@ -1,3 +1,124 @@
+
+$('.addPR').click(function() {
+ 
+    $('.customer_records').clone().appendTo('.customer_records_dynamic');
+    $('.customer_records_dynamic .customer_records').addClass('single remove');
+    $('.single .extra-fields-customer').remove();
+    // $('.single').append('<a href="#" class="remove-field btn-remove-customer">Remove Fields</a>');
+    $('.customer_records_dynamic > .single').attr("class", "remove");
+
+    $('.customer_records_dynamic input').each(function() {
+        var count =2;
+        var fieldname = $(this).attr("name");
+        var field = fieldname.slice(0, -1);
+        $(this).attr('name', field + count);
+        count++;
+      });
+
+  /*  $('.customer_records_dynamic select').each(function() {
+       var count =2;
+        var fieldname = $(this).attr("name");
+        var field = fieldname.slice(0, -1);
+        $(this).attr('name', field + count);
+         count++;
+      
+      });
+  */
+});
+
+$(document).on('click', '.remove-field', function(e) {
+  $(this).parent('.remove').remove();
+  e.preventDefault();
+});
+
+
+function proceed(){
+       
+    var data = $("#receiveHead").serialize();
+    var loc= document.getElementById("baseurl").value;
+    var redirect = loc+"receive/add_receive_head_process";
+    var conf = confirm('Are you sure you want to proceed?');
+    if(conf){
+          $.ajax({
+            data: data,
+            type: "POST",
+            url: redirect,
+            dataType: "json",
+            success: function(response){
+                  
+                var x = document.getElementById("myDIV");
+                var pr = document.getElementById("PRDiv");
+                var proc = document.getElementById("proc");
+                var cancel = document.getElementById("cancel");
+                if (x.style.display === "none" && pr.style.display === "none" ) {
+                    x.style.display = "block";
+                    pr.style.display = "block";
+                    proc.style.display = "none";
+                    cancel.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                    pr.style.display = "none";
+                    proc.style.display = "block";
+                    cancel.style.display = "none";
+                }
+
+                document.getElementById("receive_id").value  = response.receive_id;
+            }
+        });
+
+         
+    }
+
+}
+
+function cancel_receive(){
+    var id = document.getElementById("receive_id").value; 
+    var loc= document.getElementById("baseurl").value;
+    var redirect = loc+"receive/cancel_receive";
+    var conf = confirm('Are you sure you want to cancel receive transaction?');
+    if(conf){
+          $.ajax({
+            data: "id="+id,
+            type: "POST",
+            url: redirect,
+            success: function(response){
+                  
+                location.reload(true);
+            }
+        });
+
+         
+    }
+
+}
+
+function updateDetails(val){
+   var id = document.getElementById("receive_id").value; 
+   var loc= document.getElementById("baseurl").value;
+   var redirect = loc+"receive/update_receive_details";
+    $.ajax({
+            data: "id="+id+"&val="+val,
+            type: "POST",
+            url: redirect,
+            dataType: "json",
+            success: function(response){
+                  
+                var x = document.getElementById("myDIV");
+                var pr = document.getElementById("PRDiv");
+                if (x.style.display === "none" && pr.style.display === "none" ) {
+                    x.style.display = "block";
+                    pr.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                    pr.style.display = "none";
+                }
+
+                document.getElementById("receive_id").value  = response.receive_id;
+            }
+        });
+}
+ 
+
 function add_receive_items(baseurl) {
     window.open(baseurl+"index.php/receive/add_receive_item", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=50,left=350,width=700,height=600");
 }
