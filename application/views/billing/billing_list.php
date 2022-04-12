7b9630ceefd52253f9fcd314ac27370fdfc09c6c
hennetanan <hentanan.cenpri>
2022-04-12 16:32:25 +0800

    fix not functioning button

--- application/views/billing/billing_list.php
+++ application/views/billing/billing_list.php
@@ -213,6 +213,7 @@
 </div>
         
 
-
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/billing.js"></script> 
 
 
