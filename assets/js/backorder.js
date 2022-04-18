cde579f0933a3e21ff98cbb15febe6af34cecd9a
unknown <glennpaul.cenpri@gmail.com>
2022-04-18 09:12:28 -0700

    fixed errors in backorder

--- assets/js/backorder.js
+++ assets/js/backorder.js
@@ -8,8 +8,16 @@ function loadBackorder(){
 function changeQty(count){
    var cost = document.getElementById("item_cost"+count).value;
    var qty = document.getElementById("quantity"+count).value;
+   var avail_qty= document.getElementById("avail_qty"+count).value;
+  if (parseInt(qty) > parseInt(avail_qty)){
+    document.getElementById("savebutton").disabled = true;
+    alert("Quantity encoded exceeds backorder quantity!");
+  }else{
+    document.getElementById("savebutton").disabled = false;
+  }
+
    var item_total = parseFloat(cost) * parseFloat(qty);
-    document.getElementById("total_cost").innerHTML  =item_total;
+  document.getElementById("total_cost"+count).innerHTML  =item_total;
 }
 
 function changePrice(count){
@@ -20,7 +28,19 @@ function changePrice(count){
 }
 
 function saveBO(){
-  
+  var receive_date = document.getElementById("receive_date").value;
+  var po_no = document.getElementById("po_no").value;
+  var dr_no = document.getElementById("dr_no").value;
+  var si_no = document.getElementById("si_no").value;
+  if(receive_date==""){
+      alert('Received Date must not be empty!');
+  }  else if (po_no==""){
+      alert('PO No must not be empty!');
+  }  else if (dr_no==""){
+      alert('DR No must not be empty!');
+  }  else if (si_no==""){
+      alert('SI/OR No must not be empty!');
+  }  else {
   var backorderdata = $("#backorder").serialize();
   var loc= document.getElementById("baseurl").value;
   var redirect = loc+'index.php/Back_order/saveBackorder';
@@ -41,5 +61,17 @@ function saveBO(){
            
           }
         });
-      }   
+      }
+    }   
+}
+
+function check_backroder_qty(count){
+  var avail_qty= document.getElementById("avail_qty"+count).value;
+  var quantity= document.getElementById("quantity"+count).value;
+  if (parseInt(quantity) >parseInt(avail_qty)){
+    document.getElementById("savebutton").disabled = true;
+    alert("Quantity encoded exceeds backorder quantity!");
+  }else{
+    document.getElementById("savebutton").disabled = false;
+  }
 }
