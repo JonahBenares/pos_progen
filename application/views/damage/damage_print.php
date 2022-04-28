480ff735a33187804db0d1384f1b2452763c51fe
unknown <jonahbenares.cenpri@gmail.com>
2022-04-28 15:10:10 +0800

    damage and repair qty readonly

--- application/views/damage/damage_print.php
+++ application/views/damage/damage_print.php
@@ -63,28 +63,29 @@
                         <br>
                     </td>
                 </tr>
+                <?php foreach($head as $h){ ?>
                 <tr>
                     <td colspan="3">PDR No.:</td>
-                    <td colspan="7" class="bor-btm1"></td>
+                    <td colspan="7" class="bor-btm1"><?php echo $h['pdr_no']; ?></td>
                     <td colspan="1"></td>
                     <td colspan="3" align="right">Date:</td>
                     <td colspan="7" class="bor-btm1"> &nbsp;</td>
                 </tr>               
                 <tr>
                     <td colspan="3">Reported by:</td>
-                    <td colspan="7" class="bor-btm1"></td>
+                    <td colspan="7" class="bor-btm1"><?php echo $h['reported_by']; ?></td>
                     <td colspan="1"></td>
                     <td colspan="3" align="right">Accounted to:</td>
-                    <td colspan="7" class="bor-btm1"> &nbsp;</td>
+                    <td colspan="7" class="bor-btm1"> &nbsp; <?php echo $h['accounted_person']; ?></td>
                 </tr>
                 <tr>
                     <td colspan="3">Date/ Time Reported:</td>
-                    <td colspan="7" class="bor-btm1"></td>
+                    <td colspan="7" class="bor-btm1"><?php echo date("Y-m-d H:i:s", strtotime($h['date_reported'])); ?></td>
                     <td colspan="1"></td>
                     <td colspan="3" align="right">Person using upon damage: </td>
-                    <td colspan="7" class="bor-btm1">&nbsp;</td>
+                    <td colspan="7" class="bor-btm1">&nbsp; <?php echo $h['person_using']; ?></td>
                 </tr>
-
+             <?php } ?>
                 <tr>
                     <td colspan="20" ><br></td>
                 </tr>
@@ -92,37 +93,40 @@
                     <td colspan="1" style="background:#efefef"></td>
                     <td colspan="19" style="background:#efefef">Item Specification</td>
                 </tr>
+                <?php foreach($details AS $d){ ?>
                 <tr>
                     <td colspan="1"></td>
                     <td colspan="3">Item Description:</td>
-                    <td colspan="15" class="bor-btm1"></td>
+                    <td colspan="15" class="bor-btm1"><?php echo $d['item']; ?></td>
                     <td colspan="1"></td>
                 </tr>
                 <tr>
                     <td colspan="1"></td>
                     <td colspan="3">Brand:</td>
-                    <td colspan="15" class="bor-btm1"></td>
+                    <td colspan="15" class="bor-btm1"><?php echo $d['brand']; ?></td>
                     <td colspan="1"></td>
                 </tr>
                 <tr>
                     <td colspan="1"></td>
                     <td colspan="3">Serial No:</td>
-                    <td colspan="6" class="bor-btm1"></td>
+                    <td colspan="6" class="bor-btm1"><?php echo $d['serial_no']; ?></td>
                     <td colspan="3" align="right">Part No:</td>
-                    <td colspan="6" class="bor-btm1"></td>
+                    <td colspan="6" class="bor-btm1"><?php echo $d['part_no']; ?></td>
                     <td colspan="1"></td>
                 </tr>
                 <tr>
                     <td colspan="1"></td>
                     <td colspan="3">Accquisition Date:</td>
-                    <td colspan="6" class="bor-btm1"></td>
+                    <td colspan="6" class="bor-btm1"><?php echo date("F j, Y", strtotime($d['acquisition_date'])); ?></td>
                     <td colspan="3" align="right">Accquisition Cost:</td>
-                    <td colspan="6" class="bor-btm1"></td>
+                    <td colspan="6" class="bor-btm1"><?php echo number_format($d['acquisition_cost'],2); ?></td>
                     <td colspan="1"></td>
                 </tr>
+            <?php } ?>
                 <tr>
                     <td colspan="20" align="center"><br><br></td>
                 </tr>
+                  <?php foreach($head as $h){ ?>
                 <tr>
                     <td colspan="1"></td>
                     <td colspan="18">
@@ -131,7 +135,7 @@
                                 <td>Description of the Damaged on the Item:</td>
                             </tr>
                             <tr> 
-                                <td><br><br></td>
+                                <td><br><?php echo $h['damage_description']; ?><br></td>
                             </tr>
                         </table>
                     </td>
@@ -145,7 +149,7 @@
                                 <td>Reason of Damage:</td>
                             </tr>
                             <tr> 
-                                <td><br><br></td>
+                                <td><br><?php echo $h['damage_reason']; ?><br></td>
                             </tr>
                         </table>
                     </td>
@@ -157,7 +161,7 @@
                 <tr>
                     <td colspan="1"></td>
                     <td colspan="3">Inspected by:</td>
-                    <td colspan="7" class="bor-btm1"></td>
+                    <td colspan="7" class="bor-btm1"><?php echo $h['inspected_by']; ?></td>
                     <td colspan="3"></td>
                     <td colspan="4" align="right">Repairable:</td>
                     <td colspan="1" class="bor-btm1" align="center"></td>
@@ -166,7 +170,7 @@
                 <tr>
                     <td colspan="1"></td>
                     <td colspan="3">Date Inspected:</td>
-                    <td colspan="7" class="bor-btm1"></td>
+                    <td colspan="7" class="bor-btm1"><?php echo $h['date_inspected']; ?></td>
                     <td colspan="3"></td>
                     <td colspan="4" align="right">Beyond Repair:</td>
                     <td colspan="1" class="bor-btm1" align="center"></td>
@@ -183,7 +187,7 @@
                                 <td>Provide a recommendation on how the parts/equipment is going to be repaired or replaced:</td>
                             </tr>
                             <tr> 
-                                <td><br><br></td>
+                                <td><br><?php echo $h['recommendation']; ?><br></td>
                             </tr>
                         </table>
                     </td>
@@ -200,7 +204,7 @@
                                 <td>Notes:</td>
                             </tr>
                             <tr> 
-                                <td><br><br></td>
+                                <td><br><?php echo $h['remarks']; ?><br></td>
                             </tr>
                         </table>
                     </td>
@@ -223,13 +227,14 @@
                 </tr>
                 <tr>
                     <td></td>
-                    <td colspan="5" class="bor-btm1" align="center"></td>
+                    <td colspan="5" class="bor-btm1" align="center"><?php echo $h['prepared_by']; ?></td>
                     <td></td>
-                    <td colspan="5" class="bor-btm1" align="center"></td>
+                    <td colspan="5" class="bor-btm1" align="center"><?php echo $h['checked_by']; ?></td>
                     <td></td>
-                    <td colspan="6" class="bor-btm1" align="center"></td>
+                    <td colspan="6" class="bor-btm1" align="center"><?php echo $h['noted_by']; ?></td>
                     <td></td>
                 </tr>
+            <?php } ?>
                 <tr>
                     <td></td>
                     <td colspan="5" align="center" style="vertical-align:text-top;">Sales Officer</td>
