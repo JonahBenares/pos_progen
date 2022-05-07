665e9a877becf4c9a7853cf69fb2ac241ccc0095
unknown <glennpaul.cenpri@gmail.com>
2022-05-07 16:06:19 -0700

    export summary scgp

--- application/views/reports/summary_scgp.php
+++ application/views/reports/summary_scgp.php
@@ -33,9 +33,15 @@
                                     <a href="<?php echo base_url(); ?>report/print_monthly_report" class="btn btn-gradient-info btn-sm btn-rounded">
                                         <b><span class="mdi mdi-printer"></span> Print</b>
                                     </a>                            
-                                    <button type="button" class="btn btn-gradient-warning btn-sm btn-rounded" data-toggle="modal" data-target="#updateSales">
+                                   <?php if($from!='' || $to!='' || $client!=''){ ?>                         
+                                    <a href="<?php echo base_url(); ?>reports/export_summary_scgp/<?php echo $from;?>/<?php echo $to;?>/<?php echo $client;?>" class="btn btn-gradient-warning btn-sm btn-rounded">
                                         <b><span class="mdi mdi-export"></span> Export</b>
-                                    </button>
+                                    </a>
+                                    <?php } else { ?>
+                                    <a href="<?php echo base_url(); ?>reports/export_summary_scgp/" class="btn btn-gradient-warning btn-sm btn-rounded">
+                                        <b><span class="mdi mdi-export"></span> Export</b>
+                                    </a>
+                                    <?php } ?>
                                 </div>
                             </div>
                         </div>
