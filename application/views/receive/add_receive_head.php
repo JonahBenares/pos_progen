0305202afa09f40631c82ca6a9abacdb3b4cb540
unknown <jonahbenares.cenpri@gmail.com>
2022-05-11 14:48:15 +0800

    prevent f5 in receive

--- application/views/receive/add_receive_head.php
+++ application/views/receive/add_receive_head.php
@@ -1,4 +1,8 @@
-
+<script type="text/javascript">   
+function disableF5(e) { if ((e.which || e.keyCode) == 116) e.preventDefault(); };
+$(document).bind("keydown", disableF5);
+$(document).on("keydown", disableF5);
+</script>
 <!-- <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.js"></script> -->
 <div class="main-panel">
     <div class="content-wrapper">    
