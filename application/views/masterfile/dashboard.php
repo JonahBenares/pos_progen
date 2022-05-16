174dc32909de0178e68c6fdaa4b58c13fc5a9034
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2022-05-16 18:35:17 +0800

    dashboard expriy

--- application/views/masterfile/dashboard.php
+++ application/views/masterfile/dashboard.php
@@ -15,18 +15,18 @@
             </nav>
         </div>
         <div class="row">
-            <div class="col-md-4 stretch-card">
-                <div class="card bg-gradient-danger card-img-holder text-white">
-                    <div class="card-body">
-                        <img src="<?php echo base_url(); ?>assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-                        <h4 class="font-weight-normal mb-3">Weekly Sales <i class="mdi mdi-chart-line mdi-24px float-right"></i>
-                        </h4>
-                        <h2 class="mb-5">$ 15,0000</h2>
-                        <h6 class="card-text">Increased by 60%</h6>
-                    </div>
+            <div class="col-md-6 stretch-card">
+              <a href="<?php echo base_url(); ?>reports/expired_inventory" class="card bg-gradient-danger card-img-holder text-white" style="text-decoration: none;" >
+                <div class="card-body">
+                    <img src="<?php echo base_url(); ?>assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
+                    <h4 class="font-weight-normal mb-3">Near Expiry Products </h4>
+                    <i class="mdi mdi-timer float-right" style="position: absolute;font-size: 100px;top: 0;right: 0;margin: 25px;"></i>
+                    <h2 class="mb-5"> 99,099</h2>
+                    <h6 class="card-text">Click to View</h6>
                 </div>
+              </a>
             </div>
-            <div class="col-md-4 stretch-card">
+            <!-- <div class="col-md-4 stretch-card">
                 <div class="card bg-gradient-primary card-img-holder text-white">
                     <div class="card-body">
                         <img src="<?php echo base_url(); ?>assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
@@ -36,17 +36,17 @@
                         <h6 class="card-text">Decreased by 10%</h6>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 stretch-card">
-                <div class="card bg-gradient-success card-img-holder text-white">
+            </div> -->
+            <div class="col-md-6 stretch-card">
+                <a href="<?php echo base_url(); ?>backorder/backorder_form" class="card bg-gradient-success card-img-holder text-white" style="text-decoration: none;" >
                     <div class="card-body">
                         <img src="<?php echo base_url(); ?>assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-                        <h4 class="font-weight-normal mb-3">Visitors Online <i class="mdi mdi-diamond mdi-24px float-right"></i>
-                        </h4>
+                        <h4 class="font-weight-normal mb-3">Sales Back Order </h4>
+                        <i class="mdi mdi-replay float-right" style="position: absolute;font-size: 100px;top: 0;right: 0;margin: 25px;"></i>
                         <h2 class="mb-5">95,5741</h2>
-                        <h6 class="card-text">Increased by 5%</h6>
+                        <h6 class="card-text">Click to View</h6>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
         <hr>
