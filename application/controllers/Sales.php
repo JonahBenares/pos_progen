96e4efae94490385a65698db66440ca71cc031ce
unknown <glennpaul.cenpri@gmail.com>
2023-01-27 13:50:56 +0800

    dr no series based on sales date

--- application/controllers/Sales.php
+++ application/controllers/Sales.php
@@ -406,11 +406,11 @@ class Sales extends CI_Controller {
     public function goods_dr_series(){
         $sales_date=$this->input->post('sales_date');
         $year_series=date('Y', strtotime($sales_date));
-        $rows=$this->super_model->count_custom_where("sales_good_head","create_date LIKE '$year_series%'");
+        $rows=$this->super_model->count_custom_where("sales_good_head","sales_date LIKE '$year_series%'");
         if($rows==0){
              $dr_no = "PROBCD-".$year_series."-DR-0001";
         } else {
-            $maxdr_no=$this->super_model->get_max_where("sales_good_head", "dr_no","create_date LIKE '$year_series%'");
+            $maxdr_no=$this->super_model->get_max_where("sales_good_head", "dr_no","sales_date LIKE '$year_series%'");
             $drno = explode('-',$maxdr_no);
             $series = $drno[3]+1;
             if(strlen($series)==1){
@@ -431,11 +431,11 @@ class Sales extends CI_Controller {
     public function services_dr_series(){
         $sales_date=$this->input->post('sales_date');
         $year_series=date('Y', strtotime($sales_date));
-        $rows=$this->super_model->count_custom_where("sales_services_head","create_date LIKE '$year_series%'");
+        $rows=$this->super_model->count_custom_where("sales_services_head","sales_date LIKE '$year_series%'");
         if($rows==0){
              $dr_no = "PROBCD-".$year_series."-AR-0001";
         } else {
-            $maxdr_no=$this->super_model->get_max_where("sales_services_head", "dr_no","create_date LIKE '$year_series%'");
+            $maxdr_no=$this->super_model->get_max_where("sales_services_head", "dr_no","sales_date LIKE '$year_series%'");
             $drno = explode('-',$maxdr_no);
             $series = $drno[3]+1;
             if(strlen($series)==1){
